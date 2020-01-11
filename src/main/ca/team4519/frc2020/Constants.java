c1d7989e06a6b889d459c03ddea0237cd054b857
Connor Adams <ceeman.adams@gmail.com>
2020-01-11 00:25:24 -0500

    Creation of Constants File - fill w/ basic Values

---
+++ src/main/ca/team4519/frc2020/Constants.java
@@ -0,0 +1,29 @@
+package frc.ca.team4519.frc2020;
+
+public class Constants {
+
+    //Drivebase
+
+    public static final int driveNeoCurrentLimit = 10; //In Amps
+        //Drive Motors
+    public static final int leftDriveNeoA = 1;  
+    public static final int leftDriveNeoB = 0; 
+        //Shifter
+    public static final int shifter = 0;    
+        //Encoders
+    public static final int leftDriveGrayhillA = 0;  
+    public static final int leftDriveGrayhillB = 1;  
+    public static final int rightDriveGrayhillA = 2; 
+    public static final int rightDriveGrayhillB = 3;
+
+    public static final boolean isLeftDriveGrayhillFlipped = false;
+    public static final boolean isRightDriveGrayhillFlipped = false;
+
+
+
+
+
+
+
+
+}
