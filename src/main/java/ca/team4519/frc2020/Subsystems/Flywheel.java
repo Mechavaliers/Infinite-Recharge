15d10e5fe4a09f86af1613ef6d939832656caeb8
saynt <saynt@SAYNTHINIE>
2020-02-01 11:45:06 -0500

    adding constructors

--- src/main/java/ca/team4519/frc2020/Subsystems/Flywheel.java
+++ src/main/java/ca/team4519/frc2020/Subsystems/Flywheel.java
@@ -3,10 +3,20 @@ package ca.team4519.frc2020.Subsystems;
 import ca.team4519.lib.Subsystem;
 import ca.team4519.lib.Thread;
 
+import com.revrobotics.CANEncoder;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel;
+
 public class Flywheel extends Subsystem implements Thread{
 
 	 private static Flywheel thisInstance;
-	
+	 
+	 private final CANSparkMax rightWheelNeoA;
+	 private final CANEncoder rightWheelNeoAEncoder;
+	 private final CANSparkMax leftWheelNeoB;
+	 private final CANEncoder leftWheelNeoBEncoder;
+	 
+	 
 	 public synchronized static Flywheel GrabInstance()
     {
 
