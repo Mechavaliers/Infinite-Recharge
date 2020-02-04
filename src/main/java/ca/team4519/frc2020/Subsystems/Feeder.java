21f7a118096ba90added9420d27b86c148ed2532
Eternal05 <60528262+Eternal05@users.noreply.github.com>
2020-02-03 19:53:53 -0500

    balldetector
    method

--- src/main/java/ca/team4519/frc2020/Subsystems/Feeder.java
+++ src/main/java/ca/team4519/frc2020/Subsystems/Feeder.java
@@ -1,11 +1,39 @@
 package ca.team4519.frc2020.subsystems;
 
 import ca.team4519.lib.Subsystem;
+import ca.team4519.frc2020.Gains;
 import ca.team4519.lib.Thread;
+import edu.wpi.first.wpilibj.Ultrasonic;
+
+//this sensor detects if there's a ball underneath the feeder, shoudl return some sort of distance and if distance is short, be ready for intake
 
 public class Feeder extends Subsystem implements Thread{
 
-	 private static Feeder thisInstance;
+    private Ultrasonic balldetector;
+
+     private static Feeder thisInstance;
+     
+     private Feeder() {
+   
+        balldetector = new Ultrasonic(0,1); //make a method that returns a boolean if less than this distance (Gains.Feeder). Everything here might be wrong, delete if necessary
+
+
+     }
+
+     public boolean checkDistance() {
+
+        double xdistance;
+        
+        xdistance = balldetector.getRangeInches();
+
+        if (xdistance >= Gains.Feeder.BallDetectorRange) {
+            return true;
+        }
+        else {
+            return false;
+        }
+
+        }
 	
 	 public synchronized static Feeder GrabInstance()
     {
