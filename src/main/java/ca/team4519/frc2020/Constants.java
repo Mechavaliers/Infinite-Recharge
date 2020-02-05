4205df9732b3ad2f1256aa53a057cfb8ba5a45d0
Connor Adams <connor.adams@uoit.net>
2020-02-05 00:53:59 -0500

    Removed Unused

--- src/main/java/ca/team4519/frc2020/Constants.java
+++ src/main/java/ca/team4519/frc2020/Constants.java
@@ -13,23 +13,25 @@ public class Constants {
     public static final int rightDriveNeoB = 3;
         //Shifter
     public static final int shifter = 0;    
-        //Encoders
-    public static final int leftDriveGrayhillA = 0;  
-    public static final int leftDriveGrayhillB = 1;  
-    public static final int rightDriveGrayhillA = 2; 
-    public static final int rightDriveGrayhillB = 3;
 
     public static final boolean isLeftDriveGrayhillFlipped = false;
     public static final boolean isRightDriveGrayhillFlipped = false;
 
-    //Flywheel
+    //Turret
     public static final int rightWheelNeo = 4;
     public static final int leftWheelNeo = 5;
     public static final int flywheelNeoCurrentLimit = 60;
+    //Turret Sensors
+    public static final int turretEncoderA = 2;
+    public static final int turretEncoderB = 3;
+    public static final int turretLimitSwitchL = 0;
+    public static final int turretLimitSwitchR = 1;
+    public static final int turretHomeDetector = 2;
 
     //Intake
     public static final int intakeLinkageMotor = 0; //TODO Get CANID of talonSRX
     public static final int IntakeArmPot = 0;
+
     public static final int intakeLinkageEncoderA = 0;
     public static final int intakeLinkageEncoderB = 1;
 
