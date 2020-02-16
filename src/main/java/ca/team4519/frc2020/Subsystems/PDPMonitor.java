accb90fd7c3654d8faee989a6d9425e568a246d5
Connor Adams <connor.adams@uoit.net>
2020-02-15 19:34:44 -0500

    Feed the logger

--- src/main/java/ca/team4519/frc2020/Subsystems/PDPMonitor.java
+++ src/main/java/ca/team4519/frc2020/Subsystems/PDPMonitor.java
@@ -1,6 +1,7 @@
 package ca.team4519.frc2020.subsystems;
 
 import ca.team4519.frc2020.Gains;
+import ca.team4519.lib.MechaLogger;
 import edu.wpi.first.wpilibj.PowerDistributionPanel;
 
 public class PDPMonitor {
@@ -111,4 +112,31 @@ public class PDPMonitor {
         return pdp.getCurrent(Gains.PDP.TurretPivot);
     }
 
+    public void FeedLogger()
+    {
+        MechaLogger.grabInstance().logThis_Double("PDP_Total_Current", () -> getTotalCurrent());
+        MechaLogger.grabInstance().logThis_Double("Robot_Voltage", () -> getVoltage());
+        MechaLogger.grabInstance().logThis_Double("Robot_Power", () -> getTotalPower());
+        MechaLogger.grabInstance().logThis_Double("LeftDriveNeoA_Current", () -> getLeftDriveNeoA_Current());
+        MechaLogger.grabInstance().logThis_Double("LeftDriveNeoB_Current", () -> getLeftDriveNeoB_Current());
+        MechaLogger.grabInstance().logThis_Double("LeftDriveTotal_Current", () -> getLeftDriveMotorsTotalCurrent());
+        MechaLogger.grabInstance().logThis_Double("RightDriveNeoA_Current", () -> getRightDriveNeoA_Current());
+        MechaLogger.grabInstance().logThis_Double("RightDriveNeoB_Current", () -> getRightDriveNeoB_Current());
+        MechaLogger.grabInstance().logThis_Double("RightDriveTotal_Current", () -> getRightDriveMotorsTotalCurrent());
+        MechaLogger.grabInstance().logThis_Double("DrivebaseTotal_Current", () -> getDrivebaseTotalCurrent());
+        MechaLogger.grabInstance().logThis_Double("RightFlywheel_Current", () -> getRightFlywheel_Current());
+        MechaLogger.grabInstance().logThis_Double("LeftFlywheel_Current", () -> getLeftFlywheel_Current());
+        MechaLogger.grabInstance().logThis_Double("FlywheelTotal_Current", () -> getFlywheelTotalCurrent());
+        MechaLogger.grabInstance().logThis_Double("IntakeRoller_Current", () -> getIntakeRoller_Current());
+        MechaLogger.grabInstance().logThis_Double("IntakePivot_Current", () -> getIntakePivot_Current());
+        MechaLogger.grabInstance().logThis_Double("LowerFeeder_Current", () -> getLowerFeeder_Current());
+        MechaLogger.grabInstance().logThis_Double("UpperFeederCurrent", () -> getUpperFeeder_Current());
+        MechaLogger.grabInstance().logThis_Double("TurretPivot_Current", () -> getTurretPivot_Current());
+
+    }
+
+    public void update()
+    {
+        FeedLogger();
+    }
 }
