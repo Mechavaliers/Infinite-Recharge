f774d934efd30e2a3e2320a440d39d8f1a7e8f1a
Connor Adams <connor.adams@uoit.net>
2020-02-16 14:55:16 -0500

    mega formatting

--- src/main/java/ca/team4519/frc2020/Gains.java
+++ src/main/java/ca/team4519/frc2020/Gains.java
@@ -2,128 +2,136 @@ package ca.team4519.frc2020;
 
 public class Gains{
 
-	public static double CONTROL_LOOP_TIME = 0.005;	//200hz
-	public static double NEO_TicksPerRev = 42.0;
+	public static final double CONTROL_LOOP_TIME = 0.005;	//200hz
+	public static final double NEO_TicksPerRev = 42.0;
 
 	public static final class Drive {
 		private Drive() {}
 		
-		public static boolean Shifter_HIGH_GEAR = true;
-		public static boolean Shifter_LOW_GEAR = false;
+		public static final boolean Shifter_HIGH_GEAR = true;
+		public static final boolean Shifter_LOW_GEAR = false;
 		
-		public static double HANDLING_MODIFIER = 1.0;	//TODO Update this
-		public static double PATH_TOLLERANCE = 0.25;	//TODO Update this
+		public static final double HANDLING_MODIFIER = 1.0;	//TODO Update this
+		public static final double PATH_TOLLERANCE = 0.25;	//TODO Update this
 
-		public static double NEO_CorrectedTicksPerRev = NEO_TicksPerRev + 5; // TODO Put the high gear ratio in here, only used in auto...for now?
+		public static final double NEO_CorrectedTicksPerRev = NEO_TicksPerRev + 5; // TODO Put the high gear ratio in here, only used in auto...for now?
 		
 		//	Inches/Seconds
-		public static double ROBOT_MAX_VELOCITY= 180.0;			//
-		public static double ROBOT_MAX_ACCELERATION = 45.0;	//TODO Update this
-		public static double ROBOT_MAX_JERK = 23; //TOT UPDATE THis
-		public static double ROBOT_MAX_ROTATIONAL_VELOCITY = 360.0;	//TODO Update this
-		public static double ROBOT_MAX_ROTATIONAL_ACCELERATION = 250.0;	//TODO Update this
-		public static double Wheelbase_Width = 21.868502;	//Inches
-		public static double Wheelbase_Length = 39.25; 	//TODO Update this
-		public static double WheelSize_Inches = 6.250;
-		public static double EncoderTicksPerRev =(( 2 * Math.PI * WheelSize_Inches ) / NEO_CorrectedTicksPerRev);	
+		public static final double ROBOT_MAX_VELOCITY= 180.0;			//
+		public static final double ROBOT_MAX_ACCELERATION = 45.0;	//TODO Update this
+		public static final double ROBOT_MAX_JERK = 23; //TOT UPDATE THis
+		public static final double ROBOT_MAX_ROTATIONAL_VELOCITY = 360.0;	//TODO Update this
+		public static final double ROBOT_MAX_ROTATIONAL_ACCELERATION = 250.0;	//TODO Update this
+		public static final double Wheelbase_Width = 21.868502;	//Inches
+		public static final double Wheelbase_Length = 39.25; 	//TODO Update this
+		public static final double WheelSize_Inches = 6.250;
+		public static final double EncoderTicksPerRev =(( 2 * Math.PI * WheelSize_Inches ) / NEO_CorrectedTicksPerRev);	
 	
-		public static double Dist_P = 0.0;	//TODO Tune this
-		public static double Dist_I = 0.0;	//TODO tune this
-		public static double Dist_D = 0.0;	//TODO Tune this
-		public static double Dist_V = 1 / ROBOT_MAX_VELOCITY;			//CHANGE AT COMP
-		public static double Dist_A = 0;	//TODO Tune this
-		public static double DistTurn_P = 1.0 / 22.5;	//TODO Tune this
-		public static double DistTurn_I = 0.0;	//TODO Tune this
-		public static double DistTurn_D = 0.0;	//TODO Tune this
-		public static double Dist_Tollerance = 0.0;	// + or - target distance
+		public static final double Dist_P = 0.0;	//TODO Tune this
+		public static final double Dist_I = 0.0;	//TODO tune this
+		public static final double Dist_D = 0.0;	//TODO Tune this
+		public static final double Dist_V = 1 / ROBOT_MAX_VELOCITY;			//CHANGE AT COMP
+		public static final double Dist_A = 0;	//TODO Tune this
+		public static final double DistTurn_P = 1.0 / 22.5;	//TODO Tune this
+		public static final double DistTurn_I = 0.0;	//TODO Tune this
+		public static final double DistTurn_D = 0.0;	//TODO Tune this
+		public static final double Dist_Tollerance = 0.0;	// + or - target distance
 
 		//This works in Radians
-		public static double Turn_P = 0.0175; //0.0825	//TODO Tune this
-		public static double Turn_I = 0.0;	//TODO Tune this
-		public static double Turn_D = 0.00; // 0.015	//TODO Tune this
-		public static double Turn_V = 1 / 300; //0.05	//TODO Tune this
-		public static double Turn_A = 0.0; //0.00225	//TODO Tune this
-		public static double Turn_F = 0.01; //TODO Tune This
-		public static double Turn_Tollerance = 0.0225; 
+		public static final double Turn_P = 0.0175; //0.0825	//TODO Tune this
+		public static final double Turn_I = 0.0;	//TODO Tune this
+		public static final double Turn_D = 0.00; // 0.015	//TODO Tune this
+		public static final double Turn_V = 1 / 300; //0.05	//TODO Tune this
+		public static final double Turn_A = 0.0; //0.00225	//TODO Tune this
+		public static final double Turn_F = 0.01; //TODO Tune This
+		public static final double Turn_Tollerance = 0.0225; 
 	}
 
 	public static final class Intake {
 		private Intake() {}
 
-		public static double IntakeSpeed = 0.0;	//TODO Tune This
+		public static final double IntakeSpeed = 0.0;	//TODO Tune This
 	
-		public static double LinkagePot_MinVolage = 0.337;	//TODO Tune This
-		public static double LinkagePot_MaxVoltage = 0.355;	//TODO Tune This
-		public static double LinkagePot_MinAngle = 0.0;	//TODO Tune This
-		public static double LinkagePot_MaxAngle = 90.0;	//TODO Tune This
+		public static final double LinkagePot_MinVolage = 0.337;	//TODO Tune This
+		public static final double LinkagePot_MaxVoltage = 0.355;	//TODO Tune This
+		public static final double LinkagePot_MinAngle = 0.0;	//TODO Tune This
+		public static final double LinkagePot_MaxAngle = 90.0;	//TODO Tune This
 
-		public static double LinkagePos_Stowed = 1.0;	//TODO Tune This
-		public static double LinkagePos_Deployed = 87.0;	//TODO Tune This
-		public static double LinkagePos_DeployedOnAngle = 45.0;	//TODO Tune This
+		public static final double LinkagePos_Stowed = 1.0;	//TODO Tune This
+		public static final double LinkagePos_Deployed = 87.0;	//TODO Tune This
+		public static final double LinkagePos_DeployedOnAngle = 45.0;	//TODO Tune This
 
-		public static double LINKAGE_MAX_VELOCITY = 0.0;	//TODO Tune This
-		public static double LINKAGE_MAX_ACCELERATION = 0.0;	//TODO Tune This
+		public static final double LINKAGE_MAX_VELOCITY = 0.0;	//TODO Tune This
+		public static final double LINKAGE_MAX_ACCELERATION = 0.0;	//TODO Tune This
 
-		public static double Linkage_P = 0.0;	//TODO Tune This
-		public static double Linkage_I = 0.0;	//TODO Tune This
-		public static double Linkage_D = 0.0;	//TODO Tune This
-		public static double Linkage_V = 0.0;	//TODO Tune This
-		public static double Linakge_A = 0.0;	//TODO Tune This
-		public static double Linkage_Tollerance = 0.0;	//TODO Tune This
+		public static final double Linkage_P = 0.0;	//TODO Tune This
+		public static final double Linkage_I = 0.0;	//TODO Tune This
+		public static final double Linkage_D = 0.0;	//TODO Tune This
+		public static final double Linkage_V = 0.0;	//TODO Tune This
+		public static final double Linakge_A = 0.0;	//TODO Tune This
+		public static final double Linkage_Tollerance = 0.0;	//TODO Tune This
 	}
 
 	public static final class Turret {
 		private Turret() {}
 
-		public static double TURRET_MAX_VELOCITY = 0.0;	//TODO Tune This
-		public static double TURRET_MAX_ACCELERATION = 0.0;	//TODO Tune This
+		public static final double TURRET_MAX_VELOCITY = 0.0;	//TODO Tune This
+		public static final double TURRET_MAX_ACCELERATION = 0.0;	//TODO Tune This
 
-		public static double Turret_P = 0.0;	//TODO Tune This
-		public static double Turret_I = 0.0;	//TODO Tune This
-		public static double Turret_D = 0.0;	//TODO Tune This
-		public static double Turret_V = 0.0;	//TODO Tune This
-		public static double Turret_A = 0.0;	//TODO Tune This
-		public static double Turret_Tollerance = 0.0;	//TODO Tune This
+		public static final double Turret_P = 0.0;	//TODO Tune This
+		public static final double Turret_I = 0.0;	//TODO Tune This
+		public static final double Turret_D = 0.0;	//TODO Tune This
+		public static final double Turret_V = 0.0;	//TODO Tune This
+		public static final double Turret_A = 0.0;	//TODO Tune This
+		public static final double Turret_Tollerance = 0.0;	//TODO Tune This
 
-		public static double turretAngle_ConvertedHigh = -180.0;	//TODO Tune this
-		public static double turretAngle_ConvertedLow = 90.0;	//TODO Tune This
-		public static double turretAngle_EncoderHigh = 1465.0;	//TODO Tune this
-		public static double turretAngle_EncoderLow = -722.0;	//TODO Tune This
-		public static double turretAngle_Zero = 0.0;
+		public static final double turretAngle_ConvertedHigh = -180.0;	//TODO Tune this
+		public static final double turretAngle_ConvertedLow = 90.0;	//TODO Tune This
+		public static final double turretAngle_EncoderHigh = 1465.0;	//TODO Tune this
+		public static final double turretAngle_EncoderLow = -722.0;	//TODO Tune This
+		public static final double turretAngle_Zero = 0.0;
 
-		public static double slope = (turretAngle_ConvertedHigh - turretAngle_ConvertedLow)/(turretAngle_EncoderHigh - turretAngle_EncoderLow);
-		public static double offset = (turretAngle_EncoderLow - slope * turretAngle_EncoderHigh);
-
-
-		public static double Intent_RightConverted = 6;	//TODO Get Value from turret after its calibrated
-		public static double Intent_ForwardConverted = 5;	//TODO Get Value from turret after its calibrated
-		public static double Intent_ReverseConverted = 56;	//TODO Get Value from turret after its calibrated
-		public static double Intent_LeftConverted = 65;
+		public static final double slope = (turretAngle_ConvertedHigh - turretAngle_ConvertedLow)/(turretAngle_EncoderHigh - turretAngle_EncoderLow);
+		public static final double offset = (turretAngle_EncoderLow - slope * turretAngle_EncoderHigh);
 
 
+		public static final double Intent_RightConverted = 6;	//TODO Get Value from turret after its calibrated
+		public static final double Intent_ForwardConverted = 5;	//TODO Get Value from turret after its calibrated
+		public static final double Intent_ReverseConverted = 56;	//TODO Get Value from turret after its calibrated
+		public static final double Intent_LeftConverted = 65;
 	}
+
+	public static final class Flywheel
+	{
+		private Flywheel() {}
+
+		public static final double Flywheel_P = 0.0;	//TODO Tune this
+		public static final double Flywheel_I = 0.0;	//TODO Tune This
+		public static final double Flywheel_D = 0.0;	//TODO Tune This
+	}
+
 	public static final class Feeder {
 		private Feeder() {}
-		public static double BallDetectorRange = 0.0; //TODO Tune This if it even becomes a thing
+		public static final double BallDetectorRange = 0.0; //TODO Tune This if it even becomes a thing
 
 	}
 
 	public static final class PDP {
 		private PDP() {}
 
-		public static final int LeftDriveNeoA = 0;
-		public static final int LeftDriveNeoB = 0;
-		public static final int RightDriveNeoA = 0;
-		public static final int RightDriveNeoB = 0;
+		public static final int LeftDriveNeoA = 0;	//TODO Tune This
+		public static final int LeftDriveNeoB = 0;	//TODO Tune This
+		public static final int RightDriveNeoA = 0;	//TODO Tune This
+		public static final int RightDriveNeoB = 0;	//TODO Tune This
 
-		public static final int IntakeRollers = 0;
-		public static final int IntakePivot = 0;
-		public static final int FeederLower = 0;
-		public static final int FeederUpper = 0;
+		public static final int IntakeRollers = 0;	//TODO Tune This
+		public static final int IntakePivot = 0;	//TODO Tune This
+		public static final int FeederLower = 0;	//TODO Tune This
+		public static final int FeederUpper = 0;	//TODO Tune This
 
-		public static final int TurretPivot = 0;
-		public static final int LeftFlywheel = 0;
-		public static final int RightFlywheel = 0;
+		public static final int TurretPivot = 0;	//TODO Tune This
+		public static final int LeftFlywheel = 0;	//TODO Tune This
+		public static final int RightFlywheel = 0;	//TODO Tune This
 		
 	}
 }
