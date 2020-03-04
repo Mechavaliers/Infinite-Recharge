00a1923ec78e78d7fa17c72a920d7b1b25504b93
Connor Adams <connor.adams@uoit.net>
2020-03-04 00:23:40 -0500

    constants are almost constant

--- src/main/java/ca/team4519/frc2020/Constants.java
+++ src/main/java/ca/team4519/frc2020/Constants.java
@@ -16,7 +16,7 @@ public class Constants
     public static final int shifter = 0;    
 
     //Turret
-    public static final int turretPivotMotor = 9; //TODO Get CANID of talonSRX
+    public static final int turretPivotMotor = 9;
     public static final int rightWheelNeo = 2;
     public static final int leftWheelNeo = 3;
     public static final int flywheelNeoCurrentLimit = 90;
@@ -28,22 +28,22 @@ public class Constants
     public static final int turretHomeDetector = 9;
 
     //Intake
-    public static final int intakeLinkageMotor = 8; //TODO Get CANID of talonSRX
-    public static final int intakeRoller = 11; //TODO Get CANID of talonSRX
-    public static final int intakePivot = 1; //TODO probably - nicole
+    public static final int intakeLinkageMotor = 8;
+    public static final int intakeRoller = 11;
+    public static final int intakePivot = 1; //TODO rename - this controls wheel of fortune
 
 
     //Feeder
     public static final int intakeLinkageWheel = 0;
-    public static final int feeder1 = 10; //nicole - don't know if this should go here
-    public static final int feeder2 = 7; //same as above, ok this isn't working uuper?
+    public static final int feeder1 = 10; //nicole - don't know if this should go here //TODO Rename upper and lower accordingly
+    public static final int feeder2 = 7; //same as above, ok this isn't working uuper?  //TOD Rename upper and lower accordingly
     public static final int ballDetector = 2;
 
      //Climber
-    public static final int winchMotorL = 0;    //TODO Update with actual canid
-    public static final int winchMotorR = 6;    //TODO Update with actual canid
-    public static final int lockReleaseL = 2;         //TODO Update with actual canid
-	public static final int lockReleaseR = 3;         //TODO Update with actual canid
+    public static final int winchMotorL = 0;
+    public static final int winchMotorR = 1;
+    public static final int lockReleaseL = 2;
+	public static final int lockReleaseR = 3;
 
 
 }
