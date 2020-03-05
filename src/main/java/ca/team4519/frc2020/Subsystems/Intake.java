5cb86be7dd5c42eb0c5e3a99c830a5d6414577d5
Connor Adams <connor.adams@uoit.net>
2020-03-05 02:26:01 -0500

    the off is actually out

--- src/main/java/ca/team4519/frc2020/Subsystems/Intake.java
+++ src/main/java/ca/team4519/frc2020/Subsystems/Intake.java
@@ -63,10 +63,14 @@ public class Intake extends Subsystem implements Thread
         intakeLinkageMotor.set(ControlMode.PercentOutput, input);
     }
 
-    public void wantIntake(boolean on, int ballCount){
+    public void wantIntake(boolean on, boolean off, int ballCount){
         if(on && ballCount != 5){
             intakeRoller.set(ControlMode.PercentOutput, 0.8);
             intakePivot.set(true);
+        }
+        else if(off)
+        {
+            intakeRoller.set(ControlMode.PercentOutput, -0.8);
         }
         else 
         {
